5ba9365267c582affa3b639874e19fe271d4ccd8
Felix Vollmer <Felix.Vollmer@informatik.uni-luebeck.de>
2014-01-10 11:46:38 +0100

    use mean instead of sum to calculate the mean sdt derivation of the color calibration

--- Software/matlab/color_calibration_new.m
+++ Software/matlab/color_calibration_new.m
@@ -1,6 +1,6 @@
 % GregtagMacbeth ColorChecker scanned values from printout (by line)
 %    1  2   3   4   5   6   7    8   9   10  11  12  13  14  15  16  17  18  21  22
-X = [99 153 75  83  80  69  237  38  208 87  144 224 28  51  186 228 190 20  157 115
+X1 = [99 153 75  83  80  69  237  38  208 87  144 224 28  51  186 228 190 20  157 115
      20 120 128 106 96  174 121  97  97  77  179 160 85  131 65  181 102 119 173 125
      39 125 176 90  137 191 92   173 123 118 99  103 153 96  87  90  151 177 185 137];
 
@@ -16,10 +16,11 @@ T = [1100 2257 924  929 1212 1162 3105 642  2431 856 1752 2937 567 819 2135 3044
 
 
 % calculate matrix to transform to 8 bit sRGB values
-M1 = X*pinv(T);
+M1 = X1*pinv(T);
+
 
 % GregtagMacbeth ColorChecker sRGB reference values from http://www.babelcolor.com
-X = [116 81 67   % 1
+X2 = [116 81 67   % 1
      199 147 129 % 2
      91 122 156  % 3
      90 108 64   % 4
@@ -40,13 +41,13 @@ X = [116 81 67   % 1
      161 163 163 % 21
      121 121 122 % 22
 	 ];
-X = X';
+X2 = X2';
 
 % calculate matrix to transform to 8 bit sRGB values	 
-M2 = X*pinv(T);
+M2 = X2*pinv(T);
 
-sum(std((X-M1*T)'))
-sum(std((X-M2*T)'))
+mean(std((X1-M1*T)'))
+mean(std((X2-M2*T)'))
 
 %use M to calculate sRGB values for other sensor raw values
 %rgb = M*[r_raw;g_raw;b_raw]
